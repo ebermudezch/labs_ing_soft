b4814c3bfd8d69434fdf89a482e5752d65c2e70b
Enrique Bermudez <enrique.bermudez@ucr.ac.cr>
2025-08-18 15:17:13 -0600

    Archivos base laboratorio 1

---
+++ Laboratorio1_B81083/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_B81083.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
